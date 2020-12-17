1f112da5690ea3cbedd3017071d2c63c757c7466
VvencesS <DucSoanVences99@gmail.com>
2020-12-17 17:34:42 +0700

    Vong doi cua  1 Component trong RN

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
-import VerticalScrollView from './components/14_VerticalScrollView';
-import HorizontalScrollView from './components/15_HorizontalScrollView';
-import ViewPagerExample from './components/16_ViewPagerAndroid';
-import BasicFlatList from './components/17_BasicFlatList';
-import HorizontalFlatList from './components/23_HorizontalFlatList';
-import BasicSectionList from './components/24_BasicSectionList';
+// import VerticalScrollView from './components/14_VerticalScrollView';
+// import HorizontalScrollView from './components/15_HorizontalScrollView';
+// import ViewPagerExample from './components/16_ViewPagerAndroid';
+// import BasicFlatList from './components/17_BasicFlatList';
+// import HorizontalFlatList from './components/23_HorizontalFlatList';
+// import BasicSectionList from './components/24_BasicSectionList';
+import LifeCycleComponent from './components/25_LifeCycleComponent';
 
-AppRegistry.registerComponent(appName, () => BasicSectionList);
+AppRegistry.registerComponent(appName, () => LifeCycleComponent);
